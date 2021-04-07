f8f54dd999019e199651d9f7cba0250f8694426e
Tim <tim.parkerd@gmail.com>
2021-04-06 19:22:49 -0500

    v1.6.4

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Topp-Roots-Lab/xrcap',
-    version='1.6.3',
+    version='1.6.4',
     zip_safe=False,
 )
